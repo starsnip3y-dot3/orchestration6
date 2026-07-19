20d14c1fc235d9e7ec7dadf29e75e9f810c41db0
anr091 <anr00091@gmail.com>
2026-07-19 12:08:32 +0700

    Adding mock 123 untuk testing rollback schenario (ROLLBACK FLOW) beberapa masih ada yang tak tulis seenaknya di variableny, bisa dirapihin kalo mau, sisanya all good sih tak liat)

--- src/main/java/com/example/orchestration/OrchestrationApplication.java
+++ src/main/java/com/example/orchestration/OrchestrationApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrchestrationApplication {
 
+	// happy flow/roll back dah tak buat -> endpointny saga?simulateError=(boolean)
+	// <-- testing, tak pikirin kykny mock 3 gausah dibuat rollback handler karena
+	// kalo 1 atau 2 dah error, mock 3 gabakal di exec/commit karena final step juga
 	public static void main(String[] args) {
 		SpringApplication.run(OrchestrationApplication.class, args);
 	}
--- src/main/java/com/example/orchestration/controller/OrchestrationController.java
+++ src/main/java/com/example/orchestration/controller/OrchestrationController.java
@@ -2,10 +2,14 @@ package com.example.orchestration.controller;
 
 import com.example.orchestration.model.StepResponse;
 import com.example.orchestration.service.OrchestrationService;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/orchestration")
@@ -15,6 +19,18 @@ public class OrchestrationController {
 
     public OrchestrationController(OrchestrationService orchestrationService) {
         this.orchestrationService = orchestrationService;
+    }
+
+    // endpoint testing simulateErrorny, kalau mau di ubah gas aja Anggota 5
+    @PostMapping("/saga")
+    public ResponseEntity<StepResponse> runSaga(
+            @RequestParam(value = "simulateError", defaultValue = "false") boolean simulateError) {
+        StepResponse res = orchestrationService.executeSagaWorkFlow(simulateError);
+        if ("FAILED".equals(res.getStatus())) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+        }
+
+        return ResponseEntity.ok(res);
     }
 
     @PostMapping("/step3")
