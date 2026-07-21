014cad3ff5c4e9a0a3738247ad69442e47119404
ChrizZz <christian.j3r3my@gmail.com>
2026-07-21 23:04:17 +0700

    update StepResponse

--- src/main/java/com/example/orchestration/model/StepResponse.java
+++ src/main/java/com/example/orchestration/model/StepResponse.java
@@ -1,40 +1,15 @@
 package com.example.orchestration.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StepResponse {
     private String step;
     private String status;
     private String message;
     private Object payload;
-
-    public String getStep() {
-        return step;
-    }
-
-    public void setStep(String step) {
-        this.step = step;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getPayload() {
-        return payload;
-    }
-
-    public void setPayload(Object payload) {
-        this.payload = payload;
-    }
 }
